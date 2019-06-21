56f9284cdfc78280fd4a3e30c9519edaa2d2164c
appdevgenie <appdevgenie@gmail.com>
2019-06-21 09:30:48 +0200

    fix webView not showing content bug

--- app/src/main/java/com/appdevgenie/wikipedia/activities/ArticleDetailActivity.kt
+++ app/src/main/java/com/appdevgenie/wikipedia/activities/ArticleDetailActivity.kt
@@ -1,10 +1,15 @@
 package com.appdevgenie.wikipedia.activities
 
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
+import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.appdevgenie.wikipedia.R
 import com.appdevgenie.wikipedia.activities.models.WikiPage
@@ -26,7 +31,8 @@ class ArticleDetailActivity : AppCompatActivity(){
 
         web_view = findViewById(R.id.article_detail_webview)
 
-        val wikiPageJson = intent.getStringExtra("page")
+        /*val wikiPageJson = intent.getStringExtra("page")
+        Toast.makeText(this, wikiPageJson, Toast.LENGTH_LONG).show()
         currentPage = Gson().fromJson<WikiPage>(wikiPageJson, WikiPage::class.java)
         web_view?.webViewClient = object : WebViewClient(){
 
@@ -35,8 +41,33 @@ class ArticleDetailActivity : AppCompatActivity(){
             }
         }
 
+        web_view?.loadUrl(currentPage!!.fullurl)*/
+
+        val wikiPageJson = intent.getStringExtra("page")
+        //Toast.makeText(this, wikiPageJson, Toast.LENGTH_LONG).show()
+        currentPage = Gson().fromJson<WikiPage>(wikiPageJson, WikiPage::class.java)
+        web_view?.webViewClient = MyWebViewClient(this)
         web_view?.loadUrl(currentPage!!.fullurl)
 
+    }
+
+    class MyWebViewClient internal constructor(private val activity: Activity) : WebViewClient() {
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            val url: String = request?.url.toString();
+            view?.loadUrl(url)
+            return true
+        }
+
+        override fun shouldOverrideUrlLoading(webView: WebView, url: String): Boolean {
+            webView.loadUrl(url)
+            return true
+        }
+
+        override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
+            Toast.makeText(activity, "Got Error! $error", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
